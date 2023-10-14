b0010ecbba62414a0c12e870630b39a7bf6223e0
Anthony Vazquez <vazquez01@allegheny.edu>
2023-10-13 20:13:38 -0400

    artifact

--- streamlit.py
+++ streamlit.py
@@ -2,12 +2,60 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC')
 
 DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+#DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+#            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
+@st.cache_data
+def load_data(data):
+    lowercase = lambda x: str(x).lower
+    data.rename(lowercase, axis='columns', inplace=True)
+    #data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+data_load_state = st.text('Loading data...')
+total_data = pd.read_csv('data/total_2019.csv', nrows=223)
+load_data(total_data)
+capita_data = pd.read_csv('data/per_capita_2019.csv', nrows=223)
+load_data(capita_data)
+global_data = pd.read_csv('data/global_1751_2019.csv', nrows=270)
+load_data(global_data)
+data_load_state.text("Done! (using st.cache_data)")
+
+if st.checkbox('Show raw data for total CO2 emissions from fossil fuels'):
+    st.subheader('Raw data(thousand metric tons)')
+    st.write(total_data)
+
+if st.checkbox('Show raw data for total global C02 emissions per capita'):
+    st.subheader('Raw data(metric tons)')
+    st.write(capita_data)
+
+if st.checkbox('Show raw data for total global C02 emissions from 1751-2019'):
+    st.subheader('Raw data(million metric tons)')
+    st.write(global_data)
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 8), columns=['Year', 'Total carbon emissions from fossil fuel consumption and cement production (million metric tons of C)', 'Total carbon emissions from fossil fuel consumption and cement production (million metric tons of C)', 'Carbon emissions from liquid fuel consumption', 'Carbon emissions from gas fuel consumption', 'Carbon emissions from cement production', 'Carbon emissions from gas flaring', 'Per capita carbon emissions (metric tons of carbon; after 1949 only)'])
+st.line_chart(data=(pd.read_csv('data/global_1751_2019.csv')), x='Year', y=None, color=None, width=269, height=None, use_container_width=False)
+
+st.subheader('Number of CO2 emissions by fuel')
+hist_values = np.histogram(total_data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+
+st.subheader('Map of CO2 Emissions Global/National')
+if st.checkbox('Show map for total global CO2 emissions in 2019'):
+    st.map(total_data)
+elif st.checkbox('Show map for total global CO2 emissions per capita in 2019'):
+    st.map(capita_data)
+elif st.checkbox('Show chart for total global CO2 emissions from 1751-2019'):
+    chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['Year', 'Total carbon emissions from fossil fuel consumption and cement production (million metric tons of C)', 'Total carbon emissions from fossil fuel consumption and cement production (million metric tons of C)', 'Carbon emissions from liquid fuel consumption', 'Carbon emissions from gas fuel consumption', 'Carbon emissions from cement production', 'Carbon emissions from gas flaring', 'Per capita carbon emissions (metric tons of carbon; after 1949 only)'])
+    st.line_chart(data='data/global_1751_2019.csv', x='Year', y='CO2 Emissions', color=None, width=None, height=None, use_container_width=False)
+
+"""
+@st.cache_data
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
     lowercase = lambda x: str(x).lower()
@@ -15,13 +63,22 @@ def load_data(nrows):
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
-# Create a text element and let the reader know the data is loading.
 data_load_state = st.text('Loading data...')
-# Load 10,000 rows of data into the dataframe.
 data = load_data(10000)
-# Notify the reader that the data was successfully loaded.
-data_load_state.text('Loading data...done!')
+data_load_state.text("Done! (using st.cache_data)")
 
-@st.cache_data
-def load_data(nrows):
-    data_load_state.text("Done! (using st.cache_data)")
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
+
+st.subheader('Number of pickups by hour')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+
+# Some number in the range 0-23
+hour_to_filter = st.slider('hour', 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+st.map(filtered_data)
+"""
